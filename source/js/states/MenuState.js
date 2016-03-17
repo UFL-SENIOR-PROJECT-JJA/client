c6484411a3ad59454663c35a4ad3bbfc88b7538a
Joshua Kegley <josh1289op@gmail.com>
2016-03-16 20:56:43 -0400

    Fixed Johns bullet and added better menu

--- source/js/states/MenuState.js
+++ source/js/states/MenuState.js
@@ -16,23 +16,40 @@ Platformer.MenuState.prototype.init = function () {
 
 Platformer.MenuState.prototype.preload = function () {
     this.load.image('menubg', 'assets/images/Galaxy-Backgrounds.jpg');
+    this.load.image('overlay', 'assets/images/transparent.png');
+
     this.load.spritesheet('createGame', 'assets/images/menu/creategame.png', 192, 42);
     this.load.spritesheet('greenButton', 'assets/images/menu/greenButton.png', 635, 190);
     this.load.spritesheet('purpleButton', 'assets/images/menu/purpleButton.png', 635, 190);
+    this.load.spritesheet('blueButton', 'assets/images/menu/blueButton.png', 635, 190);
+
 
 };
 
 Platformer.MenuState.prototype.create = function () {
-    this.background = this.game.add.tileSprite(0, 0, 2880, 1800, 'menubg');
+    this.background = this.game.add.tileSprite(0, 0, 5680, 1800, 'menubg');
     this.background.autoScroll(-20, 0);
-    var backgroundScaleWidth = 4000 / this.background.texture.frame.width;
-    var backgroundScaleHeight = backgroundScaleWidth/16 * 9;
-    this.background.scale.setTo(backgroundScaleWidth, backgroundScaleHeight);
-    console.log(this.game);
+    this.background.scale.setTo(.3, .3);
+    this.background1 = this.game.add.tileSprite(0, 0, 2880, 1800, 'overlay');
+    this.background1.scale.setTo(.5, .5);
+
+
+    this.style = {
+        'font': '55px Arial',
+        'fill': 'white'
+    };
+
+    this.label = this.game.add.text(this.game.width/2, 100, "Andres Sucks", this.style);    //puts the label in the center of the button
+    this.label.stroke = '#000000';
+    this.label.strokeThickness = 6;
+    this.label.anchor.setTo( 0.5, 0.5 );
+
+
+
     var btnScale = .50;
-    this.btnCreateLobby = new LabelButton(this.game,this.game.width/2, this.game.height/2 - ((190*btnScale)/2) - 25, "greenButton", "Start game!", onClickCreateGame, this, 1, 0, 2); // button frames 1=over, 0=off, 2=down
+    this.btnCreateLobby = new LabelButton(this.game,this.game.width/4, this.game.height/2 - ((190*btnScale)/2) + 175, "greenButton", "Start Game!", onClickCreateGame, this, 1, 0, 2); // button frames 1=over, 0=off, 2=down
     this.btnCreateLobby.scale.setTo(btnScale, btnScale);
-    this.btnLobbys = new LabelButton(this.game,this.game.width/2, this.game.height/2 - ((190*btnScale)/2) + 75, "purpleButton", "Search Lobbies", onClickJoinGame, this, 1, 0, 2); // button frames 1=over, 0=off, 2=down
+    this.btnLobbys = new LabelButton(this.game,this.game.width/4 + this.game.width/2, this.game.height/2 - ((190*btnScale)/2) + 175, "blueButton", "Search Lobbies", onClickJoinGame, this, 1, 0, 2); // button frames 1=over, 0=off, 2=down
     this.btnLobbys.scale.setTo(btnScale, btnScale);
 
 
